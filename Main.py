d7c1c25af4186f258ea38b95d437dff1b23b473c
Makeev-AC19-04 <wane2001@gmail.com>
2022-04-29 21:09:09 +0300

    Добавлена кнопка
    По нажатии кнопки вместо стандартного текста отображается другой, введенный из поля ввода

--- Main.py
+++ Main.py
@@ -2,17 +2,35 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
+from kivy.uix.button import Button
+from kivy.clock import Clock
+
 
 KV = """ 
 MyBL:
         Label:
+                id: text_label
                 font_size: "30sp"
                 text: root.data_label
+        TextInput:
+                id: text_input
+                multiline: False
+                size_hint: (0.5,0.1)
+        Button:
+                id: button_label
+                text: "Кнопка"
+                bold: True
+                background_color:'#00FFCE'
+                size_hint: (1,0.5)
+                on_press: root.callback()
 """ # Включаем виджеты для верстки
 
 class MyBL(BoxLayout):
     data_label = StringProperty("Текст")
-
+    def callback(self):
+        data_label = StringProperty("Вы нажали на кнопку")
+        name = self.ids.text_input.text
+        self.ids.text_label.text = name
 
 class MyApp(App):
     running = True
@@ -23,5 +41,5 @@ class MyApp(App):
     def on_stop(self):
         self.running = False
 
-
-MyApp().run()
+app = MyApp()
+app.run()
